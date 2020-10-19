5a6e9ff2fb7832928516200fa5b9c3052c038948
gbeto19 <gbeto19@gmail.com>
2020-10-19 14:34:47 -0300

    first commit

---
+++ contador.js
@@ -0,0 +1,36 @@
+function contar() {
+
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert("[ERRO]")
+    }
+    else {
+        res.innerHTML = 'contando...<br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            window.alert('Passo invalido considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem cresccente
+            for (let c = 1; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // CONTAGEM REGRESSIVA
+            for (let c = 1; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
+}
